30f404db3abc117dae0388ea848055be84a40252
Mann U <mannu@Manassehs-Laptop.local>
2023-08-05 07:43:45 +0000

    contaianer and text props done

---
+++ components/Container/MainContainer.tsx
@@ -0,0 +1,19 @@
+import React, {FunctionComponent} from 'react';
+import styled from 'styled-components/native';
+import {colors} from '../colors';
+import {ContainerProps} from './types';
+
+const StyledView = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: 40px;
+  background: ${colors.primary};
+  justify-content: center;
+  align-items: center;
+`;
+
+const MainContainer: FunctionComponent<ContainerProps> = props => {
+  return <StyledView style={props.style}>{props.children}</StyledView>;
+};
+
+export default MainContainer;
---
+++ components/Text/BigText.tsx
@@ -0,0 +1,16 @@
+import React, {FunctionComponent} from 'react';
+import styled from 'styled-components/native';
+import {colors} from '../colors';
+import {TextProps} from './types';
+
+const StyledText = styled.Text`
+  font-size: 30px;
+  color: ${colors.black};
+  text-align: left;
+`;
+
+const BigText: FunctionComponent<TextProps> = props => {
+  return <StyledText style={props.style}>{props?.children}</StyledText>;
+};
+
+export default BigText;
